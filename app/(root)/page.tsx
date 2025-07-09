1e5ae850704188e4d8bffca5d5463a1dd3d8c59d
FelippeH <felipeminasgeraisbrasil@gmail.com>
2025-07-09 04:14:46 -0300

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <p>Hello Admin Dashboard</p>
+      <div className="flex flex-wrap items-center gap-2 md:flex-row">
+        <Button>Button</Button>
+      </div>
+    </>
+  );
+}
