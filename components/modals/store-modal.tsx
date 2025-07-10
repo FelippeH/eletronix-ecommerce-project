693e30de97dfa5353cc45ed49b9767ca2553dfb6
FelippeH <felipeminasgeraisbrasil@gmail.com>
2025-07-10 17:27:10 -0300

    feat(modal): add modal de loja com hook e provider

---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { Modal } from "@/components/ui/modal";
+
+export const StoreModal = () => {
+  const storeModal = useStoreModal();
+
+  return (
+    <Modal
+      title="Criar Loja"
+      description="Adicione uma nova loja para gerenciar produtos e caategorias"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      Teste Store
+    </Modal>
+  );
+};
