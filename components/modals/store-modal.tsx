c69e8e44c3d86278a32fb45606d40d84288b853a
FelippeH <felipeminasgeraisbrasil@gmail.com>
2025-07-15 17:37:52 -0300

    feat(store-modal): atualizar a validação do campo de nome para exigir no mínimo de 3 caracteres

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -17,7 +17,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 const formSchema = z.object({
-  name: z.string().min(1),
+  name: z.string().min(3),
 });
 
 export const StoreModal = () => {
