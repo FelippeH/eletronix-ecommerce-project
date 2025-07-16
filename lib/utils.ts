b7c45d577787f5356ed62c828c4e30fba431049e
FelippeH <felipeminasgeraisbrasil@gmail.com>
2025-07-16 16:30:24 -0300

    chore: adiciona comentários explicativos

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+/**
+ * Combina classes CSS de forma eficiente, removendo duplicatas e mesclando
+ * classes Tailwind CSS.
+ *
+ * @param inputs - Classes a serem combinadas.
+ * @returns String com as classes combinadas.
+ */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
