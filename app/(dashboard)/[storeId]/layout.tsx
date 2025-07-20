98024acaea5b805e3d66d109c97fa9ef1430a757
FelippeH <felipeminasgeraisbrasil@gmail.com>
2025-07-20 13:23:58 -0300

    feat(dashboard): add DashboardPage e layout com autenticação e redirecionamento

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,39 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb";
+
+export default async function DashboardLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { storeId: string };
+}) {
+  // autenticação com clerk
+  // usa o auth para verificar se o usuário está logado
+  const { userId } = await auth();
+
+  // se o usuário não estiver logado, ele é redirecionado para a página de login
+  if (!userId) {
+    redirect("/login");
+  }
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+
+  // usa o auth para verificar se a loja existe
+  if (!store) {
+    redirect("/");
+  }
+
+  return (
+    <>
+      <div>Menu</div>
+      {children}
+    </>
+  );
+}
