6ddbbfaac750b5391fb074e054c7f8e0c1ec03c2
FelippeH <felipeminasgeraisbrasil@gmail.com>
2025-07-20 13:30:10 -0300

    feat(store): add endpoint para criação de loja com autenticação de usuário

---
+++ app/api/stores/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+import prismadb from "@/lib/prismadb";
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = await auth();
+    const body = await req.json();
+    const { name } = body;
+
+    if (!userId) {
+      return new NextResponse("Acesso não autorizado", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("Nome é obrigatório", { status: 400 });
+    }
+
+    const store = await prismadb.store.create({
+      data: {
+        name,
+        userId,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log("[STORE_POST]", error);
+    return new NextResponse("Error interno", { status: 500 });
+  }
+}
